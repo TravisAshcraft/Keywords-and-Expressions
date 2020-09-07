299d369e5cd89d3a588463e5c741b39a6c0ed0f8
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-09-07 15:53:40 -0500

    Added my understanding of expressions

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50){
+            System.out.println("this is an expression");
+        }
+
+
+        int score = 100; // everything right of int is the expression
+        if(score > 99){ // everything in the brackets in the expresseion
+            System.out.println("You got the high score!"); // everything in the brackets is the expression
+            score = 0; // everything to the right of score is the expression
+        }
+
+
+
+    }
+}
